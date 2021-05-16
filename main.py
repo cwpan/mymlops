b02ad9c522d0781fd2aa60019fca6b46180dfda5
William <mr.williampan@gmail.com>
2021-05-16 21:20:43 +0000

    DEV v4

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import os
-
-from flask import Flask, request
-
+from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    name = request.args.get('name', 'World')
-    return f'Hello {name}!'
+def hello():
+    """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
+    return 'Hello World!'
+
+@app.route('/echo/<name>')
+def echo(name):
+    print(f"This was placed in the url: new-{name}")
+    val = {"new-name": name}
+    return jsonify(val)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
