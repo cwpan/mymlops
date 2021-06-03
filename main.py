c16c169527738d8d84a2dcc60fc337416759616e
William Pan <billpoon888@gmail.com>
2021-06-03 02:04:13 +0000

    DEV test branch enhancement

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-from flask import Flask
-from flask import jsonify
-app = Flask(__name__)
+import pandas as pd
+import numpy as np
+import sklearn
+import joblib
+from flask import Flask,render_template,request
+from sklearn.linear_model import LinearRegression
+app=Flask(__name__)
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    print("I am inside hello world")
-    return 'Hello CD World!'
+def home():
+	return render_template('home.html')
 
-@app.route('/echo/<name>')
-def echo(name):
-    print(f"This was placed in the url: new-{name}")
-    val = {"new-name": name}
-    return jsonify(val)
+@app.route('/predict',methods=['GET','POST'])
 
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+def predict():
+	if request.method =='POST':
+		print(request.form.get('var_1'))
+		print(request.form.get('var_2'))
+		print(request.form.get('var_3'))
+		print(request.form.get('var_4'))
+		print(request.form.get('var_5'))
+		try:
+			var_1=float(request.form['var_1'])
+			var_2=float(request.form['var_2'])
+			var_3=float(request.form['var_3'])
+			var_4=float(request.form['var_4'])
+			var_5=float(request.form['var_5'])
+			pred_args=[var_1,var_2,var_3,var_4,var_5]
+			pred_arr=np.array(pred_args)
+			print(pred_arr)
+			preds=pred_arr.reshape(1,-1)
+			model=open("linear_regression_model.pkl","rb")
+			lr_model=joblib.load(model)
+			model_prediction=lr_model.predict(preds)			
+			model_prediction=round(float(model_prediction),2)
+		except ValueError:
+			return "Please Enter valid values"
+	return render_template('predict.html',prediction=model_prediction)
+if __name__=='__main__':
+	app.run(host='127.0.0.1', port=8080, debug=True)
