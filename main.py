4e4bd042cb006039ae3d577f72e84f2a425c1f91
William <cwpan@users.noreply.github.com>
2021-06-03 01:21:16 -0400

    Update main.py
    code to be deployed in the code section of the serverless function

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-from flask import Flask
-from flask import jsonify
-app = Flask(__name__)
+import base64
+import requests 
+import json 
+import google.cloud 
+from requests.auth import HTTPBasicAuth
+from datetime import date, timedelta
+from google.cloud import storage
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    print("I am inside hello world")
-    return 'Hello World!'
+def get_watttimes_data():
+  login_url = 'https://api2.watttime.org/v2/login'
+  token = requests.get(login_url, auth=HTTPBasicAuth('yourid', 'yourpassword')).json()['token']
+  data_url = 'https://api2.watttime.org/v2/data'
+  headers = {'Authorization': 'Bearer {}'.format(token)}
+  params = {'ba': 'CAISO_ZP26', 
+          'starttime': '2019-01-20T16:00:00-0800', 
+          'endtime': '2019-02-20T16:15:00-0800'}
+  rsp = requests.get(data_url, headers=headers, params=params)
+  results = json.loads(rsp.text) 
+  return (results)
 
-@app.route('/echo/<name>')
-def echo(name):
-    print(f"This was placed in the url: new-{name}")
-    val = {"new-name": name}
-    return jsonify(val)
+def fetch_and_writeto_gcpstorage():
+  #ction = base64.b64decode(data['data']).decode('utf-8') # retrieve pubsub message
+    # ...
+  storage_client=storage.Client()
+  yesterday = date.today() - timedelta(1)
+  yesterday = str(yesterday) [:10]
+  #if (action == "download!"): # work is conditional on message content
+  parsed=get_watttimes_data()
+  payload = '\n'.join(json.dumps(item) for item in parsed)
+  file_name = "wattimedata_{}.json".format(yesterday.replace("-", ""))
+  storage_client.get_bucket("gcf-sources-json-us-central1") \
+    .blob(file_name) \
+    .upload_from_string(payload)
 
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+fetch_and_writeto_gcpstorage()
